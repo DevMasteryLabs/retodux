9e1ff48491319fcddfab509b98675a9a195ae2b7
Ahmed Zribi <ahmed.zribi@zeta-box.com>
2022-02-20 12:03:03 +0100

    Set up deleting todo

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
     {
       id: "1",
       title: "Todo 1",
-      description: "Desc 1"
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos officia culpa ipsum qui minima atque dolor suscipit quis cum accusamus"
     },
     {
       id: "2",
       title: "Todo 2",
-      description: "Desc 2"
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos officia culpa ipsum qui minima atque dolor suscipit quis cum accusamus"
     },
   ])
   const handleAddTodo = todo => {
@@ -28,13 +28,16 @@ function App() {
     console.log(newTodo);
     setTodos(prevTodos => [...prevTodos, newTodo])
   }
+  const handleDelete = (id) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
   return (
     <>
       <Router>
         <Navbar />
         <Container>
           <Switch>
-            <Route exact path='/' component={() => <Todos todos={todos} />} />
+            <Route exact path='/' component={() => <Todos todos={todos} onDelete={handleDelete} />} />
             <Route path='/new-todo' component={(props) => <NewTodo {...props} onAddTodo={handleAddTodo} /> } />
             <Route path='/todos/:id' component={TodoDetails} />
           </Switch>
--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
+import DeleteTodoModal from '../components/DeleteTodoModal'
 
 export default function Todos(props) {
+    const handleConfirmDelete = (id) => {
+        props.onDelete(id)
+    }
     return (
         <div className='mt-5'>
             <ListGroup>
                 {props.todos.map(todo => (
-                    <ListGroup.Item variant="primary" key={todo.id}> {todo.title} </ListGroup.Item>
+                    <ListGroup.Item 
+                        variant="primary" 
+                        key={todo.id} 
+                        className="d-flex justify-content-between align-items-center"
+                    >
+                        <span>{todo.title}</span>
+                        <div>
+                            <DeleteTodoModal todo={todo} onConfirmDelete={handleConfirmDelete} />    
+                        </div> 
+                    </ListGroup.Item>
                 ))}
             </ListGroup>
         </div>
