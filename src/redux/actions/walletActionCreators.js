6b37d3ce70498c46e4c14a815bd75503c13029ae
Ahmed Zribi <ahmedzribiahmed@gmail.com>
2022-02-24 01:06:35 +0100

    Remove wallet and playlist related redux code

--- src/redux/actions/walletActionCreators.js
+++
@@ -1,9 +0,0 @@
-export const depositMoney = (amount) => {
-    const action = { type: "deposit", payload: amount };
-    return action;
-}
-
-export const withdrawMoney = (amount) => {
-    const action = { type: "withdraw", payload: amount };
-    return action;
-}
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,5 @@
 import { combineReducers } from "redux";
 
-import walletReducer from "./walletReducer";
-import playlistReducer from "./playlistReducer";
 import todosReducer from "./todosReducer";
 
 const rootReducer = combineReducers({
